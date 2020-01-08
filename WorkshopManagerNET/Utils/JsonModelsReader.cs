33589610b5ffd687311227f7dad0cf2d64acfe10
Robert <robert.malina987@gmail.com>
2020-01-08 17:09:02 +0100

    basic cli runner add, generic json reader add

---
+++ WorkshopManagerNET/Utils/JsonModelsReader.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WorkshopManager.net.Utils
+{
+  public class JsonModelsReader<T> where T : class
+  {
+    private bool _srcFileExists = false;
+    private const string _baseDirPath = @"..\..\..\DataGenerator\sources\";
+    private string _srcPath = string.Empty;
+    public JsonModelsReader(string fileName)
+    {
+      _srcPath = $"{_baseDirPath}{fileName}";
+      if (!DoISeeTheFile())
+      {
+        Console.WriteLine(Environment.CurrentDirectory);
+        Console.WriteLine($"File with path: {_srcPath} not found...");
+      }
+      else
+      {
+        _srcFileExists = true;
+      }
+    }
+
+    private bool DoISeeTheFile()
+    {
+      return File.Exists(_srcPath);
+    }
+    public List<T> GetModels()
+    {
+      if (!_srcFileExists)
+      {
+        return new List<T>();
+      }
+      try
+      {
+        using (StreamReader reader = new StreamReader(_srcPath))
+        {
+          string json = reader.ReadToEnd();
+         return JsonConvert.DeserializeObject<List<T>>(json);
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e);
+      }
+      return new List<T>();
+    }
+  }
+}
---
+++ WorkshopManagerNET/Utils/WorkshopManagerCli.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkshopManagerNET.Model;
+
+namespace WorkshopManager.net.Utils
+{
+  class WorkshopManagerCli
+  {
+    public void Run()
+    {
+      string cmd = string.Empty;
+      while (!cmd.Equals("exit"))
+      {
+        Console.Write("WorkshopManagerCli> ");
+        cmd = Console.ReadLine();
+        switch (cmd)
+        {
+          case "list clients":
+          case "l clients":
+            {
+              var clientsReader = new JsonModelsReader<Client>("clients.sample-data.json");
+              var clients = clientsReader.GetModels();
+              foreach (Client c in clients)
+              {
+                Console.WriteLine(c.FirstName);
+              }
+              break;
+            }
+          case "exit":
+          case "q":
+            {
+              if (cmd.Equals("q"))
+              {
+                cmd = "exit";
+              }              
+              break;
+            }
+          default:
+            {
+              Console.WriteLine($"\n{cmd} is not known WorkshopManagerCli command...\n");             
+              break;
+            }
+        }
+      }
+      Console.WriteLine("WorkshopManagerCli exited, press any key to close terminal window...");
+    }
+  }
+}
