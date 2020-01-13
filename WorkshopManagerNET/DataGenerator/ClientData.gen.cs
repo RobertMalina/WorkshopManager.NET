203fe513fb111e215d6590bac8e788e449732859
Robert <robert.malina987@gmail.com>
2020-01-13 19:41:22 +0100

    random order to client matching method add

--- WorkshopManagerNET/DataGenerator/ClientData.gen.cs
+++ WorkshopManagerNET/DataGenerator/ClientData.gen.cs
@@ -1,6 +1,7 @@
 ﻿using EFCore.BulkExtensions;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WorkshopManager.net.Utils;
@@ -51,13 +52,13 @@ namespace WorkshopManager.net.DataGenerator
       set => _models = value;
     }
 
-    public async Task<bool> InsertModels()
+    public bool InsertModels()
     {
       try
       {
         using(var dbAccess = new WorkshopManagerContext())
         {
-          await dbAccess.BulkInsertAsync(Models);
+          dbAccess.BulkInsertAsync(Models);
           dbAccess.SaveChanges();
         }
         return true;
@@ -69,6 +70,44 @@ namespace WorkshopManager.net.DataGenerator
       }
     }
 
+    public void MatchRandomlyWith(Order[] orders)
+    {
+      LoadDbModels();
+      var clientIdsOfActiveOrders = new List<long>(); //-> because there shouldn't exists more than one active order (Registered || InProgress) of particullar client
+      var rand = new Random();
+      foreach (Order order in orders)
+      {
+        var client = GetRandom();
+        switch (order.Status)
+        {
+          case OrderStatusEnum.InProgress:
+          case OrderStatusEnum.Registered:
+            {
+              if (clientIdsOfActiveOrders.Contains(client.Id))
+              {
+                order.Status = OrderStatusEnum.Finished; //-> because there shouldn't exists more than one active order (Registered || InProgress) of particullar client
+              }
+              order.ClientId = client.Id;
+              order.Client = client;
+              clientIdsOfActiveOrders.Add(client.Id);              
+              break;
+            }
+          case OrderStatusEnum.Finished:
+            {
+              order.ClientId = client.Id;
+              order.Client = client;
+              break;
+            }
+          default:
+            {
+              break;
+            }
+        }
+
+      }
+    }
+
+
     public Task<bool> InsertModelsAsync()
     {
       throw new NotImplementedException();
@@ -79,7 +118,55 @@ namespace WorkshopManager.net.DataGenerator
       Models = JsonReader.GetModels().ToArray();
     }
 
-    bool IDataGenerator<Client>.InsertModels()
+    public async Task<bool> LoadDbModels()
+    {
+      try
+      {
+        using (var dbAccess = new WorkshopManagerContext())
+        {
+          Client[] clients = null;
+          clients = dbAccess.Clients.ToArray();
+          if (clients.Length == 0)
+          {
+            var insertSucceeded =  InsertModels();
+            if (insertSucceeded)
+            {
+              clients = dbAccess.Clients.ToArray();
+            }
+            else
+            {
+              Console.WriteLine("Could not insert Clients... (necessary if Order's are about to be inserted)");
+              return false;
+            }
+          }
+          Models = clients;
+          return true;
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e);
+        return false;
+      }
+    }
+
+
+    /// <summary>
+    /// WARNING
+    /// This method will not work correctly if Client rows have not re-seed'ed Id's... 
+    /// Truncate or delete with reseed required if data is vanished.
+    /// USE db-stored procedures to create/rebuild test data to avoid such troubles.
+    /// </summary>
+    /// <returns></returns>
+    Client GetRandom()
+    {
+      var randomGen = new Random();
+      long id = randomGen.Next(1, Models.Length);
+      var client = Models.FirstOrDefault(m => m.Id == id);
+      return client;
+    }
+
+    Client GetRandom<Client>()
     {
       throw new NotImplementedException();
     }
